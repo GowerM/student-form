76116dffc1e508681b5d2efc68c57f76b20d3ee1
Mollie <eg.mollie@gmail.com>
2014-02-20 10:44:10 -0800

    set-up

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+$(document).ready(function () {
+ 
+  //Adding active border selection
+  $('img').on('click', function () {
+	$(this).addClass('active');  
+  });
+  
+  //Still trying to figure out how to remove an active target in jquery
+  //Don't know if I will have that aspect figured out before the deadline, so for now, here is my submission
+  //By the way myewuportal people, students don't have time to read anything that does not pertain to their current classes, why are you making them read a pdf?
+   
+  
+});
